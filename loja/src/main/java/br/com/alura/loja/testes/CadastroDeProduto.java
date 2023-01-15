f984e3fffee9686f414567c598d9c51f1c78dd86
Max William <maxwilliam0440@gmail.com>
2023-01-15 01:14:50 -0300

    testando relacionamento ManyToOne Produto -> Categoria

--- loja/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ loja/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 
 import javax.persistence.EntityManager;
 
+import br.com.alura.loja.dao.CategoriaDao;
 import br.com.alura.loja.dao.ProdutoDao;
 import br.com.alura.loja.util.JPAUtil;
 import br.com.alura.modelo.Categoria;
@@ -12,10 +13,9 @@ import br.com.alura.modelo.Produto;
 public class CadastroDeProduto {
 
 	public static void main(String[] args) {
-		Categoria celulares = new Categoria("CELULARES");
-		
-		
+		Categoria celulares = new Categoria("CELULARES");		
 		Produto celular = new Produto("Xiaomi", "Legal", new BigDecimal("800"), celulares );
+		
 //		celular.setNome();
 //		celular.setDescricao();
 //		celular.setPreco();
@@ -27,12 +27,14 @@ public class CadastroDeProduto {
 
 		// conf de acesso ao banco
 		EntityManager em = JPAUtil.getEntityManager();
-		ProdutoDao dao = new ProdutoDao(em);
+		ProdutoDao produtoDao = new ProdutoDao(em);
+		CategoriaDao categoriaDao = new CategoriaDao(em);
 
 		// persistindo dados
 		em.getTransaction().begin();
 //		em.persist(celular);
-		dao.cadastrar(celular);
+		categoriaDao.cadastrar(celulares);
+		produtoDao.cadastrar(celular);
 		em.getTransaction().commit();
 		em.close();
 	}
