4a3cb8eadca4ee7bf93cec3b7509435aa3c811fd
Max William <maxwilliam0440@gmail.com>
2023-01-17 01:32:18 -0300

    status transient

--- loja/src/main/java/br/com/alura/loja/dao/CategoriaDao.java
+++ loja/src/main/java/br/com/alura/loja/dao/CategoriaDao.java
@@ -7,12 +7,16 @@ import br.com.alura.modelo.Categoria;
 public class CategoriaDao {
 
 	private EntityManager em;
-	
+
 	public CategoriaDao(EntityManager em) {
 		this.em = em;
 	}
-	
+
 	public void cadastrar(Categoria categoria) {
 		this.em.persist(categoria);
+	}
+
+	public void atualizar(Categoria categoria) {
+		this.em.merge(categoria);
 	}
 }
